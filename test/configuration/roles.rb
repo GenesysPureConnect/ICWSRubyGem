4b38a7d6a32a06111e15064f0f4cb33e5fc78cc9
Kevin Glinski <kevin.glinski@inin.com>
2014-08-26 10:51:44 -0400

    skipping unit tests on ci

--- test/configuration/roles.rb
+++ test/configuration/roles.rb
@@ -8,82 +8,47 @@ require 'icws/configuration/workgroups'
 require 'securerandom'
 
 class RoleTest <  MiniTest::Unit::TestCase
-=begin
 
-  def test_create_new_get_update_and_delete
-    connection = ICWS::Connection.new APPLICATIONNAME, SERVER
-    connection.connect USERID, PASSWORD
-    roleconfig = ICWS::Configuration::Roles.new connection
+    def test_get_all
+        skip ("Not running on integration server") if ENV[:CI] == true
 
-    workgroupconfig = ICWS::Configuration::Workgroups.new connection
-    workgroupid = SecureRandom.uuid;
-    newworkgroup = {}
+        connection = ICWS::Connection.new APPLICATIONNAME, SERVER
+        connection.connect USERID, PASSWORD
+        roleconfig = ICWS::Configuration::Roles.new connection
+        assert roleconfig != nil
 
-    workgroupconfig.create_new workgroupid, newworkgroup
+        allroles = roleconfig.get_all
 
-    id = SecureRandom.uuid;
-
-    newrole = {}
-    newrole["workgroups"] = []
-
-    roleworkgroup = {}
-    roleworkgroup['id'] = workgroupid
-    roleworkgroup['displayName'] = workgroupid
-
-    newrole['workgroups'].push roleworkgroup
-
-    roleconfig.create_new id, newrole
-
-    role = roleconfig.get id, "configurationId,workgroups"
-    assert_not_nil role
-
-    assert role["workgroups"].length > 0
-
-    assert role["workgroups"][0]['id'] == workgroupid
-
-    roleconfig.delete id
-    workgroupconfig.delete workgroupid
-
-    assert_raise RestClient::ResourceNotFound do
-      role = roleconfig.get id
+        assert allroles != nil
+        assert allroles.length > 0
+        assert allroles[0]["configurationId"] != nil
     end
 
-  end
-=end
-  def test_get_all
-    connection = ICWS::Connection.new APPLICATIONNAME, SERVER
-    connection.connect USERID, PASSWORD
-    roleconfig = ICWS::Configuration::Roles.new connection
-    assert roleconfig != nil
+    def test_get_all_custom_attributes
+        skip ("Not running on integration server") if ENV[:CI] == true
 
-    allroles = roleconfig.get_all
+        connection = ICWS::Connection.new APPLICATIONNAME, SERVER
+        connection.connect USERID, PASSWORD
+        roleconfig = ICWS::Configuration::Roles.new connection
+        assert roleconfig != nil
 
-    assert allroles != nil
-    assert allroles.length > 0
-    assert allroles[0]["configurationId"] != nil
-  end
+        allroles = roleconfig.get_all "configurationId,workgroups"
 
-  def test_get_all_custom_attributes
-    connection = ICWS::Connection.new APPLICATIONNAME, SERVER
-    connection.connect USERID, PASSWORD
-    roleconfig = ICWS::Configuration::Roles.new connection
-    assert roleconfig != nil
+        assert allroles!= nil
+        assert allroles.length > 0
+        assert allroles[0]["configurationId"] != nil
+        assert allroles[0].has_key? "workgroups" #workgroups must actually be configured for this role
+        assert allroles[0].has_key?("foo") == false
+    end
 
-    allroles = roleconfig.get_all "configurationId,workgroups"
-
-    assert allroles!= nil
-    assert allroles.length > 0
-    assert allroles[0]["configurationId"] != nil
-    assert allroles[0].has_key? "workgroups" #workgroups must actually be configured for this role
-    assert allroles[0].has_key?("foo") == false
-  end
-
-  def test_defaults
-    connection = ICWS::Connection.new APPLICATIONNAME, SERVER
-    connection.connect USERID, PASSWORD
-    roleconfig = ICWS::Configuration::Roles.new connection
-    assert roleconfig != nil
-    assert roleconfig.defaults != nil
-  end
+    def test_defaults
+        skip ("Not running on integration server") if ENV[:CI] == true
+    
+        connection = ICWS::Connection.new APPLICATIONNAME, SERVER
+        connection.connect USERID, PASSWORD
+        roleconfig = ICWS::Configuration::Roles.new connection
+        assert roleconfig != nil
+        assert roleconfig.defaults != nil
+    end
 
 end
